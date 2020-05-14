7d2ca7f7add5a310eac95545293ab5fa5b227d14
Cason Adams <casonadams@gmail.com>
2020-05-14 15:47:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# typora-rpm
+
+## requirements
+
+[fpm](https://github.com/jordansissel/fpm)
+
+## building
+
+```bash
+./build_typora_rpm.sh
+```
---
+++ build_typora_rpm.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+ITERATION="${1:-0}"
+
+RPM_NAME=typora
+VERSION=0.9.9.32.1
+VENDOR=Typora
+
+curl -L https://typora.io/linux/Typora-linux-x64.tar.gz | tar xzvf -
+
+
+fpm -s dir -t rpm \
+  --verbose \
+  --force \
+  --no-depends \
+  --no-auto-depends \
+  --rpm-rpmbuild-define "_build_id_links none" \
+  --rpm-auto-add-directories \
+  --rpm-compression=xz \
+  --vendor $VENDOR \
+  --iteration "$ITERATION" \
+  -n $RPM_NAME \
+  -v "$VERSION" \
+  --description "Clean MarkDown editor" \
+  --package-name-suffix $VENDOR \
+  bin/Typora-linux-x64=/usr/share/ \
+  typora.desktop=/usr/share/applications/ \
+  typora=/usr/bin/ \
+  ;
+
+rm -rf bin
